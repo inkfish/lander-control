f7dea0c998da5b7b32fea454c2d10abeb522a1f5
Ryan Govostes <ryan@ink.fish>
2024-01-22 17:44:21 -0800

    Change resistor values to match schematic

--- demo/voltage.py
+++ demo/voltage.py
@@ -13,8 +13,8 @@ pin_vsys = analogio.AnalogIn(board.A3)
 
 # Resistor values below are from the voltage divider circuits in the schematic
 adc_to_volts = lambda value, r1, r2: 3.3 * value * (r1 + r2) / (r2 * 65535)
-batt_24v = adc_to_volts(pin_24v.value, 32000, 5000)
-batt_48v = adc_to_volts(pin_48v.value, 68000, 5000)
+batt_24v = adc_to_volts(pin_24v.value, 49_900, 4990)
+batt_48v = adc_to_volts(pin_48v.value, 100_000, 4990)
 vsys = adc_to_volts(pin_vsys.value, 200_000, 100_000)
 
 print('24V battery voltage:', batt_24v, 'V')
